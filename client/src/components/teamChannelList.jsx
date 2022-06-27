8cd22eb7afb17cbf55fc539ceb7657a66c6c0a3d
Onesco <Okonkwoonesmus5@gmail.com>
2022-06-27 22:31:50 +0100

    installed some dependencies

---
+++ client/src/components/teamChannelList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { AddChannel } from '../assets'
+
+export default function TeamChannelList({children, error, type}) {
+  return (
+    <div>teamChannelList</div>
+  )
+}
