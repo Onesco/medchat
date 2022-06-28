77cb3cb785be599d6f233c8619222d74fc543f55
Onesco <Okonkwoonesmus5@gmail.com>
2022-06-28 23:02:32 +0100

    components entry point

---
+++ client/src/components/index.js
@@ -0,0 +1,7 @@
+export  {default as ChannelListContainer} from './ChannelListContainer'
+export {default as ChannelContainer} from './ChannelContainer'
+export {default as ChannelSearch} from './ChannelSearch'
+export {default as TeamChannelList} from './teamChannelList'
+export {default as TeamChannelPreview} from './teamChannelPreview'
+export {default as Auth} from './Auth'
+
