54daf5a8dea241c6a2f8a446f9023ad38197e911
Onesco <Okonkwoonesmus5@gmail.com>
2022-06-28 22:53:41 +0100

    modified the app

--- client/src/App.js
+++ client/src/App.js
@@ -5,16 +5,32 @@ import { Chat} from 'stream-chat-react';
 
 import './index.css'
 
-import {ChannelContainer, ChannelListContainer} from './components'
+import {ChannelContainer, ChannelListContainer, Auth} from './components'
 
 const apiKey =  process.env.REACT_APP_API_KEY
-const client = StreamChat.getInstance(apiKey);
+const chatClient = new StreamChat(apiKey);
+
+let authToken = false
+
+// const userToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoicHVycGxlLXNreS0wIn0.fX2_F1Y4IEC-fm5-oym1HdqEkftjPqcrIIMlemfOeCM';
+
+// chatClient.connectUser(
+//   {
+//     id: 'purple-sky-0',
+//     name: 'purple-sky-0',
+//     image: 'https://getstream.io/random_png/?id=purple-sky-0&name=purple-sky-0',
+//   },
+//   userToken,
+// );
+
 
 export default function App() {
+
+  if(!authToken) return <Auth/>
    
   return (
     <div className='flex flex-1'>
-        <Chat client={client} theme= 'team light'>
+        <Chat client={chatClient} >
             <ChannelListContainer/>
             <ChannelContainer/>
         </Chat>
