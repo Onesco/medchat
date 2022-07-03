0216c503882e22475801c9a7624cea5a7addb728
Onesco <Okonkwoonesmus5@gmail.com>
2022-07-03 15:55:47 +0100

    added Channelinner, createchannel, editchannel fiiles

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -4,4 +4,8 @@ export {default as ChannelSearch} from './ChannelSearch'
 export {default as TeamChannelList} from './teamChannelList'
 export {default as TeamChannelPreview} from './teamChannelPreview'
 export {default as Auth} from './Auth'
+export {default as ChannelInner} from './ChannelInner'
+export {default as CreateChannel} from './CreateChannel'
+export {default as EditChannel} from './EditChannel'
+export {default as TeamMessage} from './TeamMessage'
 
