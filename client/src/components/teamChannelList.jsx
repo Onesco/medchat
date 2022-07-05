653dd3dee94bf48980488a400aee267eb03aa468
Onesco <Okonkwoonesmus5@gmail.com>
2022-07-05 23:29:34 +0100

    add teamchannel list

--- client/src/components/teamChannelList.jsx
+++ client/src/components/teamChannelList.jsx
@@ -2,8 +2,31 @@ import React from 'react'
 
 import { AddChannel } from '../assets'
 
-export default function TeamChannelList({children, error, type}) {
+export default function TeamChannelList({children, error=false, loading, type}) {
+  
+  if(error){
+    return type === 'team'?(
+      <div>
+        <p>Connection error please wait a moment and try again</p>
+      </div>
+    ):null
+  }
+  if(loading){
+    return (
+      <div>
+        <p>
+          {type === 'team' ? 'Channels': "Messages"} loading ...
+        </p>
+      </div>
+    )
+  }
   return (
-    <div>teamChannelList</div>
+    <div>
+      <p>
+        {type === 'team' ? 'Channels': "Direct Messages"}
+      </p>
+      {/* {Button - add channels} */}
+      {children}
+    </div>
   )
 }
